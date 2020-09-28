d4274ea1b50b282929d4cdd2437c3999aecc4554
SkylerMart <71092892+SkylerMart@users.noreply.github.com>
2020-09-28 16:33:17 -0500

    Add files via upload

---
+++ Project2-functions/main2.js
@@ -0,0 +1,9 @@
+
+function MyFirstFunction() {
+ var number= 5; //variable 1// 
+ number += 7;
+ document.getElementById("math").innerHTML= number;
+}
+
+
+//use the += operator and call and display your fn//
