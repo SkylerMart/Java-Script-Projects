f4b1ad8e72dd20052687a843cd2eeefc060dbafe
SkylerMart <71092892+SkylerMart@users.noreply.github.com>
2020-09-28 12:11:55 -0500

    Add files via upload

---
+++ Project1-expressions-alert/main.js
@@ -0,0 +1,6 @@
+window.alert("Hello World!")
+document.write("\'You can use backslashs to asssist in writing quotation marks.\'");//using backslashes//
+var Family = "The Marts", Husband = "Skyler", Wife = "Joy";//string//
+document.write(Husband);//str variable//
+var Family = Wife.fontcolor ("blue");//changing fontcolor//
+document.write(3+3); //doing an expression//
