a8cf735390acfa7e96f523b389b963d6142d3ff7
SkylerMart <skylermart@gmail.com>
2020-12-30 16:00:20 -0600

    SlideShowUpdate

--- Project9-countdown-slideshowII/JS/slideshow.js
+++ Project9-countdown-slideshowII/JS/slideshow.js
@@ -1,6 +1,5 @@
 var slideIndex = 1;
 showSlides(slideIndex);
-
 // Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -23,6 +22,6 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
 }
